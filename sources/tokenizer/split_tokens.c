127730d85bbccf68ad07ee08d107751786e97edb
paulahemsi <paulahemsi@gmail.com>
2021-06-30 10:54:43 -0300

    var in single quotes string implemented

--- sources/tokenizer/split_tokens.c
+++ sources/tokenizer/split_tokens.c
@@ -6,15 +6,15 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 18:34:41 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/06/24 23:36:21 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/06/30 10:51:00 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	needs_expansion(char quote, char d_sign)
+static int	needs_exp(char quote)
 {
-	if ((quote == '\'') && (d_sign == '$'))
+	if (quote == '\'')
 		return (0);
 	return (1);
 }
@@ -39,8 +39,10 @@ t_token	*split_tokens(char *line)
 	{
 		while (line[i] == ' ')
 			i++;
-		if (is_btw_quotes(line, i, &token_end, line[i]) && needs_expansion(line[i], line[i + 1]))//!TOO LONG refatorar
+		if (is_btw_quotes(line, i, &token_end, line[i]) && needs_exp(line[i]))
 			add_token(line, i + 1, token_end, &token_lst);
+		else if (line[i] == SINGLE_QUOTE)
+			add_token(line, i, token_end, &token_lst);
 		else
 			add_token(line, i, find_end(line, i, &token_end), &token_lst);
 		i = token_end + 1;
