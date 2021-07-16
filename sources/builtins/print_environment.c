92987493b06c55f560633b833e9e148fb242d9d3
lrcouto <laurarccouto@gmail.com>
2021-07-16 00:42:15 -0300

    Changed print_environment to receive a fd as a parameter

--- sources/builtins/print_environment.c
+++ sources/builtins/print_environment.c
@@ -6,17 +6,17 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/15 01:48:59 by user42            #+#    #+#             */
-/*   Updated: 2021/07/15 01:54:07 by user42           ###   ########.fr       */
+/*   Updated: 2021/07/16 00:34:48 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	print_environment(t_hashmap *env)
+void	print_environment(t_hashmap *env, int fd)
 {
 	char	**env_array;
 
 	env_array = hashmap_to_env(env);
-	print_2d_array_fd(env_array, 1);
+	print_2d_array_fd(env_array, fd);
 	free_2d_array(env_array);
 }
