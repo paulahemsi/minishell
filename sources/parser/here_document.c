5fb8d89454e5039e23d87cad4526f7072c33622c
Lais Arena <laisarena@gmail.com>
2021-07-28 21:30:51 -0300

    Here doc working with pipe

--- sources/parser/here_document.c
+++ sources/parser/here_document.c
@@ -6,7 +6,7 @@
 /*   By: lfrasson <lfrasson@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 17:13:18 by lfrasson          #+#    #+#             */
-/*   Updated: 2021/07/27 21:43:54 by lfrasson         ###   ########.fr       */
+/*   Updated: 2021/07/28 21:27:29 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 void	here_doc_input(char *eof)
 {
 	int		tmp_fd;
+	int		save_fd_out;
 	char	*input;
 
 	tmp_fd = open(TMP_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0600);
@@ -26,6 +27,8 @@ void	here_doc_input(char *eof)
 		g_minishell.error_status = 1;
 		return ;
 	}
+	save_fd_out = dup(OUT);
+	dup2(g_minishell.save_fd[OUT], STDOUT_FILENO);
 	while (true)
 	{
 		input = readline("> ");
@@ -43,5 +46,7 @@ void	here_doc_input(char *eof)
 	unlink(TMP_FILE);
 	dup2(tmp_fd, IN);
 	close(tmp_fd);
+	dup2(save_fd_out, STDOUT_FILENO);
+	close(save_fd_out);
 }
 
