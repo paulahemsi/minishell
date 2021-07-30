4cec11b6cf14e5715d3653183f5c886cad1b4f3c
paulahemsi <paulahemsi@gmail.com>
2021-07-30 17:33:15 -0300

    more refact

--- sources/tokenizer/variables_expansion.c
+++ sources/tokenizer/variables_expansion.c
@@ -6,11 +6,18 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 18:25:26 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/07/30 16:57:04 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/07/30 17:32:07 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	free_auxiliary_strings(char **temp, char **aux, char **splited_str)
+{
+	ft_free_and_null((void **)temp);
+	ft_free_and_null((void **)aux);
+	free_2d_array(splited_str);
+}
 
 static void	handle_quote(t_var *var, char **value, int i)
 {
@@ -27,15 +34,7 @@ static void	handle_quote(t_var *var, char **value, int i)
 		temp = ft_strdup("\'");
 	aux = ft_substr(*value, i, (ft_strlen(*value) - i));
 	var->after = ft_strjoin(temp, aux);
-	ft_free_and_null((void **)&temp);
-	ft_free_and_null((void **)&aux);
-	i = 0;
-	while (splited_string[i])
-	{
-		ft_free_and_null((void **)&splited_string[i]);
-		i++;
-	}
-	ft_free_and_null((void **)&splited_string);
+	free_auxiliary_strings(&temp, &aux, splited_string);
 }
 
 static bool	is_exit_status_variable(char **value)
@@ -47,6 +46,17 @@ static bool	is_exit_status_variable(char **value)
 		return (TRUE);
 	}
 	return (FALSE);
+}
+
+static void	deal_with_string_slices(t_var *var, char **value, int *i)
+{
+	var->before = ft_substr(*value, 0, *i);
+	var->value = ft_substr(*value, *i, get_var_size(var->pointer, i));
+	if (ft_strchr(var->value, SINGLE_QUOTE))
+		handle_quote(var, value, *i);
+	else
+		var->after = ft_substr(*value, *i, (ft_strlen(*value) - *i));
+	expand(&var->value);
 }
 
 void	expand_variables(char **value)
@@ -61,13 +71,7 @@ void	expand_variables(char **value)
 	var.pointer = search_var(*value, &i);
 	if (!(var.pointer))
 		return ;
-	var.before = ft_substr(*value, 0, i);
-	var.value = ft_substr(*value, i, get_var_size(var.pointer, &i));
-	if (ft_strchr(var.value, SINGLE_QUOTE))
-		handle_quote(&var, value, i);
-	else
-		var.after = ft_substr(*value, i, (ft_strlen(*value) - i));
-	expand(&var.value);
+	deal_with_string_slices(&var, value, &i);
 	new_str = variadic_strjoin(3, var.before, var.value, var.after);
 	free(*value);
 	*value = new_str;
