92af03243ad6911754232abda9ef1f62bf9deead
Lais Arena <laisarena@gmail.com>
2021-08-01 13:57:25 -0300

    Check if command is a executable

--- sources/exec/path.c
+++ sources/exec/path.c
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/17 17:25:29 by lcouto            #+#    #+#             */
-/*   Updated: 2021/07/31 19:54:21 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/08/01 13:56:04 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,21 @@ static char	*get_possible_path(char *envpath, char *cmd)
 	return (path);
 }
 
-char	*get_absolute_path(char *cmd, char *path_variable)
+static bool	is_executable(char *cmd_path)
 {
 	struct stat	buffer;
+
+	if (stat(cmd_path, &buffer) != 0)
+		return (FALSE);
+	if ((buffer.st_mode & S_IFMT) == S_IFDIR)
+		return (FALSE);
+	if ((buffer.st_mode & S_IXUSR))
+		return (TRUE);
+	return (FALSE);
+}
+
+char	*get_absolute_path(char *cmd, char *path_variable)
+{
 	char		**all_paths;
 	char		*cmd_path;
 	int			i;
@@ -35,7 +47,7 @@ char	*get_absolute_path(char *cmd, char *path_variable)
 	while (all_paths && all_paths[i])
 	{
 		cmd_path = get_possible_path(all_paths[i], cmd);
-		if (stat(cmd_path, &buffer) == 0)
+		if (is_executable(cmd_path))
 			break ;
 		free(cmd_path);
 		cmd_path = NULL;
