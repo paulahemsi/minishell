63c55299278bf1b6836bfa94eab2fdcd60c122ca
Lais Arena <laisarena@gmail.com>
2021-08-04 23:09:55 -0300

    Implement here documents signals behaviors

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/27 15:09:54 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/08/04 19:56:58 by lfrasson         ###   ########.fr       */
+/*   Updated: 2021/08/05 09:11:10 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ void	command_parser(t_token *token_lst, t_token *pipe, int *old_pipe_in);
 void	check_redirects(t_token *token_lst, t_token *pipe, int *save_fd);
 void	here_doc_input(char *eof, int *save_fd);
 char	**create_command_array(t_token *head, t_token *pipe);
+void	interrupt_here_document(int signal);
 
 #endif
